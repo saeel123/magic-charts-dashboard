303ba90b261e376a753b7300efa9771886b7f418
Saeel Parulekar <saeelparulekar>
2020-06-23 01:59:10 +0530

    added chart data

--- src/containers/Chart/Chart.js
+++ src/containers/Chart/Chart.js
@@ -149,8 +149,84 @@ let dashboardData = [
                 } 
             }
         ]
+    },
+    {
+        id: 3, 
+        dId: 3, 
+        charts: [
+            {
+                id: 1, 
+                type: 'bar', 
+                data: {
+                    labels: ['January', 'February', 'March'],
+                    datasets: [
+                        {
+                            label: 'My First dataset',
+                            backgroundColor: 'rgba(255,99,132,0.2)',
+                            borderColor: 'rgba(255,99,132,1)',
+                            borderWidth: 1,
+                            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                            hoverBorderColor: 'rgba(255,99,132,1)',
+                            data: [65, 59, 80]
+                        }
+                    ]
+                } 
+            },
+            {
+                id: 2, 
+                type: 'line', 
+                data: {
+                    labels: ['January', 'February', 'March'],
+                    datasets: [
+                        {
+                            label: 'My First dataset',
+                            fill: false,
+                            lineTension: 0.1,
+                            backgroundColor: 'rgba(75,192,192,0.4)',
+                            borderColor: 'rgba(75,192,192,1)',
+                            borderCapStyle: 'butt',
+                            borderDash: [],
+                            borderDashOffset: 0.0,
+                            borderJoinStyle: 'miter',
+                            pointBorderColor: 'rgba(75,192,192,1)',
+                            pointBackgroundColor: '#fff',
+                            pointBorderWidth: 1,
+                            pointHoverRadius: 5,
+                            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                            pointHoverBorderColor: 'rgba(220,220,220,1)',
+                            pointHoverBorderWidth: 2,
+                            pointRadius: 1,
+                            pointHitRadius: 10,
+                            data: [65, 59, 80]
+                        }
+                    ]
+                } 
+            },
+            {
+                id: 3, 
+                type: 'pie', 
+                data: {
+                    labels: ['March', 'April', 'May'],
+                    datasets: [{
+                        data: [200, 20, 60],
+                        backgroundColor: [
+                        '#FF6384',
+                        '#36A2EB',
+                        '#FFCE56'
+                        ],
+                        hoverBackgroundColor: [
+                        '#FF6384',
+                        '#36A2EB',
+                        '#FFCE56'
+                        ]
+                    }]
+                } 
+            }
+        ]
     }
 ];
+
+let chartFinal = null;
 
 class Chart extends Component {
 
@@ -167,7 +243,9 @@ class Chart extends Component {
     render() {
         const dashboardDataObjects = [
             {id: 1, name: "Dash 1"},
-            {id: 2, name: "Dash 2"}
+            {id: 2, name: "Dash 2"},
+            {id: 3, name: "Dash 3"}
+
         ];
 
         let dashboardlist = dashboardDataObjects.map( dashboardDataObject => (
@@ -176,7 +254,7 @@ class Chart extends Component {
             </button>
         ));
 
-        let chartFinal = null;
+        
 
        
         if (this.state.chartData) {
@@ -185,7 +263,7 @@ class Chart extends Component {
                 switch (chart.type) {
                     case 'bar':
                         charts.push(
-                            <div>
+                            <div key={chart.id}>
                                 <h2>Bar Example (custom size)</h2>
                                 <Bar
                                     data={chart.data}
@@ -200,7 +278,7 @@ class Chart extends Component {
 
                     case 'line':
                         charts.push(
-                            <div>
+                            <div key={chart.id}>
                                 <h2>Line Example</h2>
                                 <Line data={chart.data} />
                             </div>
@@ -209,7 +287,7 @@ class Chart extends Component {
 
                     case 'pie':
                         charts.push(
-                                <div>
+                                <div key={chart.id}>
                                     <h2>Pie Example</h2>
                                     <Pie data={chart.data} />
                                 </div>
@@ -226,7 +304,11 @@ class Chart extends Component {
                   {charts}
                 </div> 
             )       
-        } 
+        } else {
+            chartFinal = (
+                <div> Please select Dash</div>
+            )
+        }
 
         return(
             <div>
